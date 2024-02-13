eedc8dec04889bfd2c40e1ab22756240b21bef82
Renat Aubakirov <arenat98@mail.ru>
2024-02-13 12:49:45 +0700

    first commit

---
+++ src/org/example/PhoneBookMain.java
@@ -0,0 +1,7 @@
+public class PhoneBookMain {
+    public static void main(String[] args) {
+        PhoneBook phoneBook = new PhoneBook();
+        PhoneBookMenu menu = new PhoneBookMenu(phoneBook);
+        menu.displayMenu();
+    }
+}
